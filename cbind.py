86999ac404ccefdb152f5a7bf46249973b7682e3
anthony-aylward <aaylward@eng.ucsd.edu>
2019-03-28 11:13:20 -0700

    Update cbind.py

--- cbind.py
+++ cbind.py
@@ -1,4 +1,2 @@
 def cbind(*data_frames):
-    df = pd.concat(data_frames, axis=1, ignore_index=True)
-    df.columns = (column_name for df in data_frames for column_name in df)
-    return df
+    return pd.concat(data_frames, axis=1)
